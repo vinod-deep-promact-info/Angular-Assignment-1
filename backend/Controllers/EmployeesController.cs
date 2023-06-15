8f7c049e2ff235fdcdbb9c976fb3a726734b6930
vinod deep <vinoddeep@promactinfo.com.com>
2023-06-15 12:42:08 +0530

    initial backend

---
+++ backend/Controllers/EmployeesController.cs
@@ -0,0 +1,103 @@
+﻿using EmployeeCRUDAPI.Data;
+using EmployeeCRUDAPI.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.Reflection;
+
+namespace EmployeeCRUDAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        private readonly DBContext _context;
+        public EmployeesController(DBContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllEmployees()
+        {
+            var emp = await _context.Employees.OrderBy(x => x.id).ToListAsync();
+
+            if (emp == null || emp?.Count == 0)
+            {
+                return NotFound("No record found!");
+            }
+
+            return Ok(emp);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Employee>> GetEmployee(int id)
+        {
+            var emp = await _context.Employees.FindAsync(id);
+
+            if (emp == null)
+            {
+                return NotFound("No record found!");
+            }
+
+            return Ok(emp);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Employee>> AddEmployee([FromBody] Employee emp)
+        {
+            if (emp == null)
+            {
+                return BadRequest("Invalid employee data");
+            }
+            else
+            {
+                await _context.Employees.AddAsync(emp);
+                await _context.SaveChangesAsync();
+                return Ok(emp);
+            }
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Employee>> EditEmployee([FromBody] Employee employee, int id)
+        {
+            if (employee == null || id != employee.id)
+            {
+                return BadRequest("Invalid employee data or ID mismatch.");
+            }
+
+            var existingEmployee = await _context.Employees.FindAsync(id);
+            if (existingEmployee == null)
+            {
+                return NotFound("Employee not found.");
+            }
+
+            existingEmployee.name = employee.name;
+            existingEmployee.email = employee.email;
+            existingEmployee.contact = employee.contact;
+            existingEmployee.gender = employee.gender;
+            existingEmployee.skills = employee.skills;
+
+            _context.Employees.Update(existingEmployee);
+            await _context.SaveChangesAsync();
+
+            return Ok(existingEmployee);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteEmployee(int id)
+        {
+            var employee = await _context.Employees.FindAsync(id);
+            if (employee == null)
+            {
+                return NotFound("Employee not found.");
+            }
+
+            _context.Employees.Remove(employee);
+            await _context.SaveChangesAsync();
+
+            return Ok(new { result = "Deleted" });
+        }
+    }
+}
---
+++ backend/Data/DBContext.cs
@@ -0,0 +1,18 @@
+﻿using EmployeeCRUDAPI.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace EmployeeCRUDAPI.Data
+{
+    public class DBContext : DbContext
+    {
+        public DBContext(DbContextOptions<DBContext> options) : base(options) { }
+
+        public DbSet<Employee> Employees { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
